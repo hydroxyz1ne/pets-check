8b24c1662135bfb008c4e9a0ddeb064a18598497
hydroxyz1ne <pluxuryluv20@gmail.com>
2024-02-04 19:03:22 +0500

    oleg ebaniat

---
+++ app/src/main/java/com/example/petscheck/PetActivity.java
@@ -0,0 +1,14 @@
+package com.example.petscheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+    }
+}
